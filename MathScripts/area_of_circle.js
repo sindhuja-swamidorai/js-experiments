fe9b4d5cdcd972682602079a3d10f02b3551bf9d
Sindhuja Swamidorai <sindhuja-swamidorai@pluralsight.com>
2024-05-13 10:18:26 +0530

    Exercice files

---
+++ MathScripts/area_of_circle.js
@@ -0,0 +1,6 @@
+radius = 0.5;
+pi = 3.14;
+
+area = pi * (radius ** 2);
+
+console.log(`The area of a circle with radius ${radius} is ${area}`);
---
+++ MathScripts/c_to_f.js
@@ -0,0 +1,5 @@
+
+c = 30;
+f = (c*9)/5 + 32;
+
+console.log(`For temperature ${c} in Celcius, value in Faranheit is ${f}`);
