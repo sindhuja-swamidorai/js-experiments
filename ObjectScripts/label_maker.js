672c169908bc0140f6c10a0f449297eaafdc1cc9
Sindhuja Swamidorai <sindhuja-swamidorai@pluralsight.com>
2024-05-14 14:04:16 +0530

    label maker

---
+++ ObjectScripts/label_maker.js
@@ -0,0 +1,14 @@
+let myInfo = {
+    name: "Mayank Gupta",
+    address: "House No. 123, 5th Street, Rajput Nagar",
+    city: "New Delhi",
+    state: "Delhi",
+    zip: "110001"
+}
+
+console.log(`
+             ${myInfo.name}
+             ${myInfo.address}
+             ${myInfo.city} - ${myInfo.zip}
+             ${myInfo.state}` )
+
